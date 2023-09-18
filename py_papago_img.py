85c980db1fe2d31a2105292c83fadeb88760e07e
KangWon LEE <kangwon@gmail.com>
2023-09-18 11:28:48 +0900

    function version

--- py_papago_img.py
+++ py_papago_img.py
@@ -4,28 +4,29 @@ import uuid
 import json
 import base64
 
-data = {
-  'source': 'ko',
-  'target': 'en',
-  'image': ('a.png', open('a.png', 'rb'), 'application/octet-stream', {'Content-Transfer-Encoding': 'binary'})
-}
-m = MultipartEncoder(data, boundary=uuid.uuid4())
 
-headers = {
-  "Content-Type": m.content_type,
-  "X-NCP-APIGW-API-KEY-ID": User_client_ID,
-  "X-NCP-APIGW-API-KEY": User_client_secret
-}
+def translate_img(image_path, src, tgt, tgt_folder, User_client_ID, User_client_secret):
+    data = {
+    'source': src,
+    'target': tgt,
+    'image': (image_path, open(image_path, 'rb'), 'application/octet-stream', {'Content-Transfer-Encoding': 'binary'})
+    }
+    m = MultipartEncoder(data, boundary=uuid.uuid4())
 
-url = " https://naveropenapi.apigw.ntruss.com/image-to-image/v1/translate"
-res = requests.post(url, headers=headers, data=m.to_string())
-print(res.text)
+    headers = {
+    "Content-Type": m.content_type,
+    "X-NCP-APIGW-API-KEY-ID": User_client_ID,
+    "X-NCP-APIGW-API-KEY": User_client_secret
+    }
 
-# renderedImage -> Image output
-resObj = json.loads(res.text)
-imageStr = resObj.get("data").get("renderedImage")
-imgdata = base64.b64decode(imageStr)
+    url = " https://naveropenapi.apigw.ntruss.com/image-to-image/v1/translate"
+    res = requests.post(url, headers=headers, data=m.to_string())
+    print(res.text)
 
-filename = 'a_translated.png'
-with open(filename, 'wb') as f:
-    f.write(imgdata)
+    # renderedImage -> Image output
+    resObj = json.loads(res.text)
+    imageStr = resObj.get("data").get("renderedImage")
+    imgdata = base64.b64decode(imageStr)
+
+    tgt_path = tgt_folder / image_path.name
+    tgt_path.wrtie_bytes(imgdata)
